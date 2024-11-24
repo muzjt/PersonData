b8b7ef8f39a024c8eeab6c81899a57ab62e20749
Jakub Muzyczuk <jtmuzyczuk@hotmail.com>
2024-11-24 22:02:42 +0100

    Add project files.

---
+++ PersonDataApp/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace PersonDataApp
+{
+    internal class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        public Person(string firstName, string lastName, int age)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+        }
+    }
+}
+
+
